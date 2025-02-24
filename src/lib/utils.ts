44985c07173db3e9f5c5c70e0c9a7f354ce4b8d8
F0rgger <164800835+F0rgger@users.noreply.github.com>
2025-02-24 06:01:48 +0200

    v1

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,5 @@
 import { User, Recipe } from "@/types/types";
 
-/**
- * Фильтрация пользователей по ID, имени или фамилии
- * @param users - список пользователей
- * @param search - строка поиска
- * @returns отфильтрованный список пользователей
- */
 export function filterUsers(users: User[], search: string): User[] {
     if (!users.length) return [];
 
@@ -23,12 +17,6 @@ export function filterUsers(users: User[], search: string): User[] {
     );
 }
 
-/**
- * Фильтрация рецептов по названию или тегам
- * @param recipes - список рецептов
- * @param search - строка поиска
- * @returns отфильтрованный список рецептов
- */
 export function filterRecipes(recipes: Recipe[], search: string): Recipe[] {
     if (!recipes.length) return [];
 
